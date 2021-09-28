f5dca42809a7bd0873aaeb19dbc44c88d6cb7887
Tolu Opeloyeru <71344094+toluopel@users.noreply.github.com>
2021-09-28 16:11:50 -0400

    Update code.py

--- code.py
+++ code.py
@@ -8,7 +8,7 @@ start_time = time.time()
 
 def take_snapshot():
     number = random.randint(0,100)
-    videoCaptureObject = cv2.VideoCapture(0, cv2.CAP)
+    videoCaptureObject = cv2.VideoCapture(0)
     result = True
     while(result):
         ret, frame = videoCaptureObject.read()
